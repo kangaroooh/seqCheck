1f302dc36e627b2104cee8777039acc7de707454
robroooh <robroo.pc@gmail.com>
2015-07-28 10:06:32 +0700

    FIRST COMMIT
    with MDL

---
+++ app.js
@@ -0,0 +1,257 @@
+// just to gen CP table and locate where to Debug
+var CPt = genCP();
+var classTable = genClassTable();
+
+// key sequence handler, use to get text from textfield then slice
+// and pass to check class function
+function ksHandler() {
+
+    dbug1 = document.getElementById("dbug1");
+    dbug2 = document.getElementById("dbug2");
+    dbug3 = document.getElementById("dbug3");
+
+    // get the fullText of Textfield id:input in index.html
+    fullText = document.getElementById("input").value;
+    fullTextLength = fullText.length;
+
+    // get all and the last key press
+    all = fullText.slice(0, fullTextLength - 1);
+    lastKey = fullText[fullTextLength - 1];
+
+    // call the function then display
+    retVal = keySequenceCheck(all, lastKey);
+
+    input.value = retVal;
+
+    dbug1.innerHTML = all;
+    dbug2.innerHTML = lastKey;
+    dbug3.innerHTML = retVal;
+}
+
+function keySequenceCheck(all, lastKey) {
+
+    
+    // the special case to check only the first to NOT having TONE and FOLLOWING Vowel infront
+    checkFirst = all.concat(lastKey).charCodeAt(0);
+
+    if ((checkFirst >= 3632 && checkFirst <= 3646) || (checkFirst >= 3653 && checkFirst <= 3662)){
+        return '';
+    }
+
+    /*
+     z = next character
+     y = previous character
+     x = character previous to y
+     */
+
+    // swap = y = [z, z = y][0];
+
+    // extract base and xyz to be processed and concat
+    base = all.slice(0, all.length - 2);
+    x = all[all.length - 2];
+    y = all[all.length - 1];
+    z = lastKey;
+
+    //convert xyz into ascii then classify
+    xClass = getClass(x.charCodeAt(0));
+    yClass = getClass(y.charCodeAt(0));
+    zClass = getClass(z.charCodeAt(0));
+
+
+    // 1st case if FV1 is in front of TONE
+    if (yClass == 4 && (zClass == 10 || zClass == 12)) {
+
+        // to handle AV with maitaiku
+        if ([10,12].indexOf(xClass) != -1 && [10,12].indexOf(zClass) != -1 ){
+            x = [z, z = x][0];
+            z = '';
+        }else{
+            y = [z, z = y][0];    
+        }
+        
+    // 2nd case if LV1, FV1, BD, TONE, AD2 has TONE, reject
+    } else if ([3, 4, 5, 13].indexOf(yClass) != -1 && [10,12,13,14,15,16].indexOf(zClass) != -1) {
+        y = [z, z = y][0];
+        z = '';
+
+    // 3rd case TONE + AV1-3 SWAP
+    } else if (yClass == 10 && [14, 15, 16].indexOf(zClass) != -1) {
+        y = [z, z = y][0];
+
+    // 4th case DUPLICATE of ALL except ctrl,non,cons , reject
+    }else if ([0, 1, 2].indexOf(yClass) == -1 && yClass == zClass) {
+        y = [z, z = y][0];
+        z = '';
+
+    // 5th case karant and sara-i
+    }else if (yClass == 11 && zClass == 14){
+        y = [z, z = y][0];
+
+    // 6th case for DUPLICATE of AV type, reject 
+    }else if ([7,8,12,13,14,15,16].indexOf(yClass) != -1 && [7,8,12,13,14,15,16].indexOf(zClass) != -1){
+        y = [z, z = y][0];
+        z = '';
+
+    // 7th case for swapping if vowel before bv
+    }else if ([10,11,12,14,15,16].indexOf(yClass) != -1 && [7,8].indexOf(zClass) != -1){
+        y = [z, z = y][0];
+        
+    // 8th case for reject above vowel with below vowel, reject
+    }else if ([7,8].indexOf(yClass) != -1 && [14,15,16].indexOf(zClass) != -1){
+        y = [z, z = y][0];
+        z = '';
+
+    // 9th case for replace of BV when it's with karant
+    }else if ([7,8].indexOf(xClass) != -1 && [10,11].indexOf(yClass) != -1 && [7,8].indexOf(zClass) != -1){
+        x = [z, z = x][0];
+        z = '';
+
+    // 10th case for replace of AV when it's with karant
+    }else if ([14,15,16].indexOf(xClass) != -1 && yClass == 11 && [14,15,16].indexOf(zClass) != -1){
+        x = [z, z = x][0];
+        z = '';
+
+    // 11th case sara-um with tone
+    }else if (xClass == 10 && y.charCodeAt(0) == 3635 && zClass == 10){
+        x = [z, z = x][0];
+        z = '';   
+    }
+
+    return base.concat(x).concat(y).concat(z);
+}
+
+function getClass(ch) {
+
+    // check for control char and delete
+    if (( ch >= 0 && ch <= 37 ) || ch == 127) {
+
+        return 0; // return 'CTRL';
+
+        // check for all English alphabets, numeral, angkhakhu, fongnam,
+        // khomut,maiyamok,baht sign
+    } else if (( ch >= 65 && ch <= 122 ) || ( ch >= 3663 && ch <= 3675 )) {
+
+        return 1; // return 'NON';
+
+        // check for all consonant Thai character
+    } else if ((ch >= 3585 && ch <= 3630)) {
+
+        return 2; // return 'CONS'
+    } else {
+
+        // if not in the above range, then look up in classTable
+        return classTable[ch];
+    }
+
+}
+
+function genClassTable() {
+
+    var table = {
+
+        // Leading Vowel return 'LV'
+        3648: 3,
+        3649: 3,
+        3650: 3,
+        3651: 3,
+        3652: 3,
+
+        // Following vowels type 1 return 'FV1'
+        3632: 4,
+        3634: 4,
+        3635: 4,
+
+        // Following vowels type 2 return 'FV2'
+        3653: 5,
+
+        // Following vowels type 3 return 'FV3'
+        3620: 6,
+        3622: 6,
+
+        // Below vowel type 1 return 'BV1' sara-u
+        3640: 7,
+
+        // Below vowel type 2 return 'BV2' sara-uu
+        3641: 8,
+
+        // BD(UNDERDOT) pinthu
+        3642: 9,
+
+        // TONE return 'TONE'
+        3656: 10,
+        3657: 10,
+        3658: 10,
+        3659: 10,
+
+        // Above Diacritics type 1 return 'AD1' karant
+        3660: 11,
+        3661: 11,
+
+        // Above Diacritics type 2 return 'AD2'
+        3655: 12,
+
+        // Above Diacritics type 3 return 'AD3'
+        3662: 13,
+
+        // Above Vowel type 1 return 'AV1'
+        3636: 14,
+
+        // Above Vowel type 2 return 'AV2'
+        3633: 15,
+        3638: 15,
+
+        // Above Vowel type 3 return 'AV3'
+        3637: 16,
+        3639: 16
+    };
+
+    return table;
+}
+function genCP() {
+    
+    var tabl = new Array(17);
+    for (var i = 0; i < 17; i++) {
+        tabl[i] = new Array(17);
+    }
+
+    // load value to array
+    s0 = ['X', 'A', 'A', 'A', 'A', 'A', 'A', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R'];
+    s1 = ['X', 'A', 'A', 'A', 'S', 'S', 'A', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R'];
+    s2 = ['X', 'A', 'A', 'A', 'A', 'S', 'A', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C', 'C'];
+    s3 = ['X', 'S', 'A', 'S', 'S', 'S', 'S', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R'];
+    s4 = ['X', 'A', 'A', 'A', 'A', 'S', 'A', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R'];
+    s5 = ['X', 'A', 'A', 'A', 'A', 'S', 'A', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R'];
+    s6 = ['X', 'A', 'A', 'A', 'S', 'A', 'S', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R'];
+    s7 = ['X', 'A', 'A', 'A', 'A', 'S', 'A', 'R', 'R', 'R', 'C', 'C', 'R', 'R', 'R', 'R', 'R'];
+    s8 = ['X', 'A', 'A', 'A', 'S', 'S', 'A', 'R', 'R', 'R', 'C', 'R', 'R', 'R', 'R', 'R', 'R'];
+    s9 = ['X', 'A', 'A', 'A', 'S', 'S', 'A', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R'];
+    s10 = ['X', 'A', 'A', 'A', 'A', 'A', 'A', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R'];
+    s11 = ['X', 'A', 'A', 'A', 'S', 'S', 'A', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R'];
+    s12 = ['X', 'A', 'A', 'A', 'S', 'S', 'A', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R'];
+    s13 = ['X', 'A', 'A', 'A', 'S', 'S', 'A', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R', 'R'];
+    s14 = ['X', 'A', 'A', 'A', 'S', 'S', 'A', 'R', 'R', 'R', 'C', 'C', 'R', 'R', 'R', 'R', 'R'];
+    s15 = ['X', 'A', 'A', 'A', 'S', 'S', 'A', 'R', 'R', 'R', 'C', 'R', 'R', 'R', 'R', 'R', 'R'];
+    s16 = ['X', 'A', 'A', 'A', 'S', 'S', 'A', 'R', 'R', 'R', 'C', 'R', 'C', 'R', 'R', 'R', 'R'];
+
+    for (var i = 0; i < 17; i++) {
+        tabl[0][i] = s0[i];
+        tabl[1][i] = s1[i];
+        tabl[2][i] = s2[i];
+        tabl[3][i] = s3[i];
+        tabl[4][i] = s4[i];
+        tabl[5][i] = s5[i];
+        tabl[6][i] = s6[i];
+        tabl[7][i] = s7[i];
+        tabl[8][i] = s8[i];
+        tabl[9][i] = s9[i];
+        tabl[10][i] = s10[i];
+        tabl[11][i] = s11[i];
+        tabl[12][i] = s12[i];
+        tabl[13][i] = s13[i];
+        tabl[14][i] = s14[i];
+        tabl[15][i] = s15[i];
+        tabl[16][i] = s16[i];
+    }
+
+    return tabl;
+}
---
+++ dumb.js
@@ -0,0 +1,77 @@
+var classToNum = {
+  'CTRL':0,
+  'NON':1,
+  'CONS':2,
+  'LV':3,
+  'FV1':4,
+  'FV2':5,
+  'FV3':6,
+  'BV1':7,
+  'BV2':8,
+  'BD':9,
+  'TONE':10,
+  'AD1':11,
+  'AD2':12,
+  'AD3':13,
+  'AV1':14,
+  'AV2':15,
+  'AV3':16
+};
+
+var blah = {
+
+  // Leading Vowel return 'LV'
+  3648:3,
+  3649:3,
+  3650:3,
+  3651:3,
+  3652:3,
+
+  // Following vowels type 1 return 'FV1'
+  3632:4,
+  3634:4,
+  3635:4,
+
+  // Following vowels type 2 return 'FV2'
+  3653:5,
+
+  // Following vowels type 3 return 'FV3'
+  3620:6,
+  3622:6,
+
+  // Below vowel type 1 return 'BV1'
+  3640:7,
+
+  // Below vowel type 2 return 'BV2'
+  3641:8,
+
+  // BD(UNDERDOT)
+  3642:9
+
+  // TONE return 'TONE'
+  3656:10,
+  3657:10,
+  3658:10,
+  3659:10,
+
+  // Above Diacritics type 1 return 'AD1'
+  3661:11,
+  3662:11,
+
+  // Above Diacritics type 2 return 'AD2'
+  3655:12,
+
+  // Above Diacritics type 3 return 'AD3'
+  3662:13,
+
+  // Above Vowel type 1 return 'AV1'
+  3636:14,
+
+  // Above Vowel type 2 return 'AV2'
+  3637:15,
+
+  // Above Vowel type 3 return 'AV3'
+  3638:16,
+
+
+};
